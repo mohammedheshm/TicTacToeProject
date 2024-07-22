e422f53f440fabcd2144ded91ff7bf33cf821aff
Mohammed Hesham <mohammedhe4am@gmail.com>
2024-07-22 23:51:30 +0300

    Create Local Page Home

---
+++ Client App/HomePage/src/splashpage/FXMLSplashController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package splashpage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import pagemanager.Navigation;
+import javafx.scene.input.MouseEvent;
+
+
+/**
+ * FXML Controller class
+ *
+ * @author user
+ */
+public class FXMLSplashController implements Initializable {
+        
+    @FXML
+    public void handleNextButton(ActionEvent  event) {
+            
+        
+       Navigation.nextPage(event, "/homepage/FXMLHome.fxml");
+       
+       
+    }
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
--- Client App/HomePage/src/splashpage/SplashPage.java
+++ Client App/HomePage/src/splashpage/SplashPage.java
@@ -3,9 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package homepage;
+package splashpage;
 
+import java.io.IOException;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,16 +17,21 @@ import javafx.stage.Stage;
  *
  * @author user
  */
-public class HomePage extends Application {
+public class SplashPage extends Application {
     
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+    public void start(Stage stage) throws IOException {
+        
+       
+
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLSplash.fxml"));
         
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
         stage.show();
+  
+    
     }
 
     /**
